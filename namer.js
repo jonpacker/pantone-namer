a9988fbc75790de9c1a810d26154871b81bb7b91
Jon Packer <contact@jonpacker.com>
2018-10-08 14:19:53 +0200

    compat

--- namer.js
+++ namer.js
@@ -2,8 +2,10 @@ const chroma = require('chroma-js');
 const pantone = require('./pantone');
 module.exports = input => {
   input = chroma(input);
-  return pantone
+  return (
+    pantone
     .map(color => ({ ...color, distance: chroma.distance(input, color.color) }))
     .sort((a, b) => a.distance - b.distance)
-    [0].name;
+    [0].name
+  );
 }
